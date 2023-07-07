e48f4d47b36cad9435f3fc7a0bf615358ad9a9ba
tushardongre1990 <128589995+tushardongre1990@users.noreply.github.com>
2023-07-08 05:19:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+document.querySelector("h1").onmouseover = (event) => {
+  let iteration = 0;
+  const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.dataset.value[index];
+        }
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+    if (iteration >= event.target.dataset.value.length) clearInterval(interval);
+    iteration += 1;
+  }, 80);
+};
